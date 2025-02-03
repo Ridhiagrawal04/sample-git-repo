6a32e46f86992f44cac8846c83086cfe77622e42
unknown <Ridhi Agrawal@RidhiAgrawal.com>
2025-02-03 17:06:57 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('basic learning')
+
+col1, col2 =st.columns(2)
+
+with col1:
+    st.image("basiclearnig.jpg")
+
+with col2:
+    st.write("""
+    Basic learning needs are those which comprise both essential learning tools (such as literacy, oral expression, numeracy, and problem solving) and the basic learning content (such as knowledge, skills, values, and attitudes) required for survival""")
+
